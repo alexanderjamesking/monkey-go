6b75827495b963915f3aac0b3427e1202c3f43fe
Alex King <alex@alexanderjamesking.com>
2020-11-05 21:40:07 +0100

    Chapter 1 - Lexer

---
+++ token/token.go
@@ -0,0 +1,47 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	// special types
+	ILLEGAL = "ILLEGAL" // unknown token / character
+	EOF     = "EOF"     // end of file
+
+	// identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	// delimiters
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
